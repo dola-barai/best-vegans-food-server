78495e4415b6aa84e15c451f2131a7891ee1b0c4
Dola Barai <dola43dc@gmail.com>
2023-05-03 17:46:34 +0600

    server added

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 3000
 
+const chefs = require('./Data/chefs.json');
+const recipe = require('./Data/recipe.json');
+app.use(cors());
+
 app.get('/', (req, res) => {
-  res.send('Food is running')
+  res.send('Food is Loading')
+})
+
+app.get('/chefs', (req, res) => {
+  res.send(chefs)
+})
+
+app.get('/recipe', (req, res) => {
+  res.send(recipe)
+})
+
+app.get('/chefs/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedChef = chefs.find(n => n.id === id); 
+  res.send(selectedChef)
 })
 
 app.listen(port, () => {
